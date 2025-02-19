d2ff59b14bfbdfc2db7aa645ed96412f699cca35
jimmerzeel <jari@brievendoos.nl>
2025-02-19 17:00:46 +0100

    Create REPL loop

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -12,5 +14,29 @@ func cleanInput(text string) []string {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	// wait for user input using bufio.NewScanner
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// start an infinite for loop
+	for {
+		// use fmt.Print to print the prompt Pokedex > WITHOUT a newline character
+		fmt.Print("Pokedex > ")
+
+		// use the scanner's .Scan and .Text methods to get the user's input as a string
+		scanner.Scan()
+		input := scanner.Text()
+
+		// clean the user's input by triming any leading or trailing whitespace, and converting it to lowercase. (strings.ToLower and strings.Fields)
+		lowerInput := strings.ToLower(input)
+		text := strings.Fields(lowerInput)
+
+		// capture the first "word" of the input and use it to print: "Your command was: <first word>"
+		firstWord := text[0]
+
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+
+	// first input: CHARMANDER is better than bulbasaur
+	// second input: Pikachu is kinda mean to ash
+	// terminate the program by pressing ctrl+c
 }
