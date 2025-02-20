de6f1550f78c74872e852edb11f52383bd3e07fd
jimmerzeel <jari@brievendoos.nl>
2025-02-20 20:13:22 +0100

    Refactor and add map command

--- repl.go
+++ repl.go
@@ -13,23 +13,6 @@ type cliCommand struct {
 	callback    func() error
 }
 
-var commands map[string]cliCommand
-
-func init() {
-	commands = map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-}
-
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -41,10 +24,10 @@ func startRepl() {
 			continue
 		}
 
-		command := text[0]
+		commandName := text[0]
 
-		if _, ok := commands[command]; ok {
-			commands[command].callback()
+		if command, ok := getCommands()[commandName]; ok {
+			command.callback()
 		} else {
 			fmt.Println("Unknown command")
 		}
@@ -58,6 +41,26 @@ func cleanInput(text string) []string {
 	return trimmed
 }
 
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays names of 20 location areas",
+			callback:    commandMap,
+		},
+	}
+}
+
 func commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
@@ -67,8 +70,18 @@ func commandExit() error {
 func commandHelp() error {
 	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
 
-	for _, v := range commands {
+	for _, v := range getCommands() {
 		fmt.Printf("%s: %s\n", v.name, v.description)
 	}
+	return nil
+}
+
+// TODO update all commands to accept a pointer to a "config" struct as a parameter.
+// This struct will contains the next and previous URLs that are necessary to paginate through the location areas
+
+func commandMap(url string) error {
+	// use pokeapi location-area endpoint to get the location areas
+	// locations, next, previous := getLocationNames(url)
+
 	return nil
 }
